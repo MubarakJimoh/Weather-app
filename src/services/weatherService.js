cdca3935274e84672de5c903a516da1ffb0c9f2d
MubarakJimoh <olajimohmj@gmail.com>
2022-12-17 22:12:01 +0000

    2nd commit

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,9 +1,9 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "ENTER YOUR API KEY";
+const API_KEY = "c7e3d600a79df3275f84db4897e53a5e";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
-// https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
+
 
 const getWeatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + "/" + infoType);
